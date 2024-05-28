ad329d554dc5aff38b33225b2178642657740d15
Viper1933 <viperzz1933@gmail.com>
2024-05-28 22:59:35 +0700

    add gen 12 room list

--- utils/customRoom.js
+++ utils/customRoom.js
@@ -12,6 +12,23 @@ const getCustomRoom = (type) => {
       daisy: "461465",
       gendis: "461476",
       michie: "461481",
+      abigail_rachel: "509985",
+      adeline_wijaya: "509992",
+      aisa_maharani_napitupulu: "509995",
+      aurhel_alana: "509997",
+      catherina_vallencia: "510000",
+      fritzy_rosmerian: "510011",
+      hillary_abigail: "510012",
+      jazzlyn_trisha: "510013",
+      letycia_moreen: "510014",
+      michelle_levia: "510016",
+      nayla_suji_aurelia: "510064",
+      nina_tutachia: "510065",
+      oline_manuel: "510067",
+      regina_wilian: "510068",
+      ribka_budiman: "510070",
+      shabilqis_naila: "510071",
+      victoria_kimberly: "510073",
     };
   } else {
     rooms = {
